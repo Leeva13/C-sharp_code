ffaad656dbde2036869335f6638467df92011800
Leeva13 <91129155+Leeva13@users.noreply.github.com>
2022-09-14 17:43:04 +0300

    Add files via upload

---
+++ laba1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double I;
+            double U;
+            double R;
+
+            while (true)
+            {
+                Console.Write("Введіть напругу. U = ");
+                string strU = Console.ReadLine();
+                try
+                {
+                    U = double.Parse(strU);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                break;
+            }
+
+            while (true)
+            {
+                Console.Write("Введіть опір. R = ");
+                string strR = Console.ReadLine();
+                try
+                {
+                    R = double.Parse(strR);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                break;
+            }
+            if (R > 0)
+            {
+                I = U / R;
+                Console.WriteLine("Сила струму = , I = {0:n2}.R = {1}", I, R);
+            }
+            else
+            {
+                Console.WriteLine("error / try again");
+            }
+            Console.ReadLine();
+        }
+    }
+}
