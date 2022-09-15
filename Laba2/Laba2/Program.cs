b2065b2350146e3a4aa9865a35276220dfc5ca85
Leeva13 <91129155+Leeva13@users.noreply.github.com>
2022-09-15 20:56:08 +0300

    Add files via upload

---
+++ Laba2/Laba2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            int nk, nn;
+
+            Console.Write("Введіть початковий елемент рядку: nn = ");
+            string strnn = Console.ReadLine();
+            nn = Int32.Parse(strnn);
+
+            Console.Write("Введіть межу ряду: nk = ");
+            string strnk = Console.ReadLine();
+            nk = Int32.Parse(strnk);
+
+            if (nn > nk || nn < 0){
+                Console.WriteLine("Введено некоректні значення!");
+                Console.Write("Введіть початок ряду = ");
+                strnn = Console.ReadLine();
+                nn = Int32.Parse(strnn);
+            }
+            double Sum = 0;
+
+            for (double i = nn; i < nk; i++)
+            {
+                Sum += ((i * i) + Math.Pow(-1, i) * i - 1) / (i * i + 1);
+            }
+
+            Console.WriteLine("Сума числового ряду = " + Sum);
+            Console.Read();
+        }
+    }
+}
