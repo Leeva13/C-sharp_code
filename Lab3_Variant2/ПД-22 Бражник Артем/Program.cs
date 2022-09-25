150dfa69d54b9585bcab7043e897f277766d4f89
Leeva13 <91129155+Leeva13@users.noreply.github.com>
2022-09-25 15:06:37 +0300

    Add files via upload

---
+++ Lab3_Variant2/ПД-22 Бражник Артем/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_2Variant
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input x:");
+            string str1 = Console.ReadLine();
+            int strX = Int32.Parse(str1);
+
+            Console.WriteLine("Input y:");
+            string str2 = Console.ReadLine();
+            int strY = Int32.Parse(str2);
+
+            if(strX < 0 && strY > 0) {
+                Console.WriteLine("True");
+            }else { 
+                Console.WriteLine("False"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
