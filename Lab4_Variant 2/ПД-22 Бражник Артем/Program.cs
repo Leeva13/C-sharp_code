a4c31767ec8bb7f7f7f84dfd6534784ab2c9662e
Leeva13 <91129155+Leeva13@users.noreply.github.com>
2022-09-25 15:31:32 +0300

    Add files via upload

---
+++ Lab4_Variant 2/ПД-22 Бражник Артем/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Variant_2
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 15,2,63,3,6,7,-3,-14,64,98 };
+            for (int i = 1; i < arr.Length; i+=2)
+            {
+                Console.Write(arr[i]+ " "); // Виводимо непарні індекси
+            }
+            Console.WriteLine("\n");
+            for (int i = 0; i < arr.Length; i += 2)
+            {
+                Console.Write(arr[i] + " "); // Виводимо парні індекси
+            }
+            Console.ReadLine();
+        }
+    }
+}
